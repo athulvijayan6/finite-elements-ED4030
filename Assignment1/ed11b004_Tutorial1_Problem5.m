3db55efdc0db54273441d1a939c782748548ec24
Athul Vijayan <athulvijayan6@gmail.com>
2014-08-16 11:52:11 +0530

    initial commit

---
+++ Assignment1/ed11b004_Tutorial1_Problem5.m
@@ -0,0 +1,16 @@
+% @Author: Athul Vijayan
+% @Date:   2014-08-16 11:45:06
+% @Last Modified by:   Athul Vijayan
+% @Last Modified time: 2014-08-16 11:46:13
+
+% We will solve the following equation using finite-differences:
+%
+% $$ \frac{d \phi}{d x} = 3 \phi + 4 $$, 
+% 
+% subject to the boundary conditon
+%
+% $$\phi = 0 \ \mathrm{at} \ x=0$$
+%
+% The solution is 
+%
+% $$\phi = \frac{4}{3}(e^(3 x) -1)$$
